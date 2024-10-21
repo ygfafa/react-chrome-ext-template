18ed396f5f0fd969c9a86fc02f52ddce78fa5334
박종혁 <jonghyeok@jobis.co>
2024-10-21 13:55:10 +0900

    Init sample code

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,7 +1,40 @@
-import { runtime } from "webextension-polyfill";
+import { runtime, storage } from "webextension-polyfill";
+import { getCurrentTab } from "../helpers/tabs";
+
+type Message = {
+  from: string;
+  to: string;
+  action: string;
+};
+
+async function incrementStoredValue(tabId: string) {
+  const data = await storage.local.get(tabId);
+  const currentValue = (data?.[tabId] ?? 0) as number;
+
+  return storage.local.set({ [tabId]: currentValue + 1 });
+}
+
+export async function init() {
+  await storage.local.clear();
+
+  runtime.onMessage.addListener(async (message) => {
+    const msg = message as Message;
+
+    if (msg.to === "background") {
+      console.log("background handled: ", msg.action);
+
+      const tab = await getCurrentTab();
+      const tabId = tab.id;
+
+      if (tabId) {
+        return incrementStoredValue(tabId.toString());
+      }
+    }
+  });
+}
 
 runtime.onInstalled.addListener(() => {
-  console.log("[background] loaded ");
+  init().then(() => {
+    console.log("[background] loaded ");
+  });
 });
-
-export {};
--- src/popup/App/App.tsx
+++ src/popup/App/App.tsx
@@ -1,7 +1,12 @@
+import { Counter } from "../Counter";
 import "./App.css";
 
 type Props = {};
 
 export const App = (props: Props) => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
 };
